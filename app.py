3a1d055608a8ae3f2bc6f08d3dddbc3641b7bcf5
AYUSH KUMAR <139534359+ayushroy2003@users.noreply.github.com>
2025-02-12 13:18:39 +0530

    Update and rename application.py to app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-application = Flask(__name__)
-app=application
+app = Flask(__name__)
+
 
 ## import ridge regresor model and standard scaler pickle
 ridge_model=pickle.load(open('models/Ayu_ridge.pkl','rb'))
